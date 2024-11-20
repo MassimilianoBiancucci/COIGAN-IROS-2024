6b1dc07dbe42329bc023b8b404aab4e38f42cf7d
SvMax <Biancucci95@Gmail.com>
2024-11-20 18:11:19 +0000

    minor config changes

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,6 +1,7 @@
 docker run -td \
-  --gpus '"device=1,2"'\
-  --shm-size=50g \
+  --name coigan_IROS2024_gpu_1 \
+  --gpus '"device=1"'\
+  --shm-size=150g \
   -v $(cat experiments_path.txt):/coigan/COIGAN-IROS-2024/experiments \
   -v $(cat datasets_path.txt):/coigan/COIGAN-IROS-2024/datasets \
   coigan-iros-2024:latest
